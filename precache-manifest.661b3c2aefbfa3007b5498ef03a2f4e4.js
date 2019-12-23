1e7fe6999bdafee91e25715254e987ac84c8b95a
Kai Hsiang Chuang <kevinshuang1029@gmail.com>
2019-12-23 12:02:34 +0800

    Updates

---
+++ precache-manifest.661b3c2aefbfa3007b5498ef03a2f4e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdb77f5e3dc25667bd43bbc1bd3e2bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dcd1e2a76dfa966646a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "94ca9a44e92f19f0a5e7",
+    "url": "/static/js/2.805f9660.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.805f9660.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dcd1e2a76dfa966646a",
+    "url": "/static/js/main.437dee24.chunk.js"
+  },
+  {
+    "revision": "49cf5d18bbd6bcffe847",
+    "url": "/static/js/runtime-main.9d6ce6bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
